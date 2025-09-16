7bdf7b5ccb930a6527f50331f0091b51aff00e56
ramzorevelo <97457734+ramzorevelo@users.noreply.github.com>
2025-09-16 18:18:02 +0800

    Update countOccurences.cpp

--- countOccurences.cpp
+++ countOccurences.cpp
@@ -4,8 +4,15 @@
 int linearSearchWithCount(const std::vector<int>& vec, int target) {
     // TODO: Count the number of occurrences of the target in the vector
     // Hint: Check each element and increment a counter
-    return 0.0; // Placeholder
-
+    int counter{ 0 };
+    //for (size_t i{ 0 }; i < vec.size(); i++ ) {
+    //    if (vec[i] == target) counter++;
+    //}
+    //for (int num : vec) {
+    //    if (num == target) counter++;
+    //}
+    //return counter; // Placeholder
+    return std::count(vec.begin(), vec.end(), target);
 }
 
 int main() {
