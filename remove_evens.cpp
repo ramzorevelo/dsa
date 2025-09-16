e76ec1e59293edaaf7fd3df77795898d5463394a
ramzorevelo <97457734+ramzorevelo@users.noreply.github.com>
2025-09-16 18:18:26 +0800

    Update remove_evens.cpp

--- remove_evens.cpp
+++ remove_evens.cpp
@@ -6,6 +6,20 @@ void removeEvens(std::vector<int>& vec) {
     // Hint: Use a loop to check each element; if even, use vec.erase()
     // Example: To remove the element at index 3, use vec.erase(vec.begin() + 3);
     // IMPORTANT: After erasing, the vector size decreases!
+    //for (int i{ 0 }; i < static_cast<int>(vec.size()); i++) {
+    //    if (!(vec[i] % 2)) {
+    //        vec.erase(vec.begin() + i);
+    //        i--;
+    //    }
+    //}
+    //int size{ static_cast<int>(vec.size()) };
+    //for (int i{ size - 1 }; i >= 0; i--) {
+    //    if (!(vec[i] % 2)) {
+    //      vec.erase(vec.begin() + i);
+    //    }
+    //}
+    std::erase_if(vec, [](int num) { return num % 2 == 0;});
+
 }
 
 int main() {
