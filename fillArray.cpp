e6ccaa5dfe0c42c6fa4c4700ed0bdefd2011eedf
ramzorevelo <97457734+ramzorevelo@users.noreply.github.com>
2025-09-16 18:17:38 +0800

    Update fillArray.cpp

--- fillArray.cpp
+++ fillArray.cpp
@@ -3,7 +3,17 @@
 #include <numeric>
 void fillArray(std::array<int, 5>& arr, int startValue) {
     // TODO: Use a loop to fill arr with consecutive values starting from 'startValue'
-    // Hint: First element is startValue
+    // Hint: Time complexity is O(n)
+    //for (int i{ 0 }; i < 5; i++) {
+    //    arr[i] = startValue++;
+    //}
+        //for (int i{ 0 }; i < 5; i++) {
+    //    arr[i] = startValue+i;
+    //}
+    //for (int& element : arr) {
+    //    element = startValue++;
+    //}
+    std::iota(arr.begin(), arr.end(), startValue);
 }
 
 int main() {
